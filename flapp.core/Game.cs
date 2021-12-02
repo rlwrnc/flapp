076b6a2fa6ee666099c4ade410c6c956d68843a8
rlwrnc <raymond.lawrence@tutanota.com>
2021-12-02 17:57:01 -0500

    pipe code & assets

--- flapp.core/Game.cs
+++ flapp.core/Game.cs
@@ -9,6 +9,7 @@ namespace flapp
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private Player player;
+        private Pipe pipe;
         public FlappGame()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -20,20 +21,22 @@ namespace flapp
         {
             // set screen size to 1920x1080
             _graphics.IsFullScreen = false;
-            _graphics.PreferredBackBufferWidth = 1920;
-            _graphics.PreferredBackBufferHeight = 1080;
+            _graphics.PreferredBackBufferWidth = 600;
+            _graphics.PreferredBackBufferHeight = 800;
             _graphics.ApplyChanges();
 
             // initialize game objects
             var pos = new Vector2(_graphics.PreferredBackBufferWidth / 2, 0);
             player = new Player(pos);
+            pipe = new Pipe(new Vector2(0, 0));
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            player.LoadContent(Content);
+            // player.LoadContent(Content);
+            pipe.LoadContent(Content);
         }
 
         protected override void Update(GameTime gameTime)
@@ -41,7 +44,7 @@ namespace flapp
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            player.Update(gameTime);
+            // player.Update(gameTime);
             base.Update(gameTime);
         }
 
@@ -51,7 +54,8 @@ namespace flapp
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
-            player.Draw(gameTime, _spriteBatch);
+            // player.Draw(gameTime, _spriteBatch);
+            pipe.Draw(gameTime, _spriteBatch);
             _spriteBatch.End();
 
             base.Draw(gameTime);
---
+++ flapp.core/Pipe.cs
@@ -0,0 +1,32 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace flapp
+{
+    public class Pipe
+    {
+        Texture2D Texture;
+        Vector2 Position;
+        public Pipe(Vector2 initialPosition)
+        {
+            Position = initialPosition;
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            Texture = content.Load<Texture2D>("rectangle");
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, Color.White);
+        }
+    }
+}
